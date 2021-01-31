d0b9cf8a49c1143fe66c3a227ed2c21bdca4d68a
Denis <dennyjuice@gmail.com>
2021-02-01 03:44:15 +0700

    fetch articles, load fullArticle, pagination

--- src/components/pages/ArticlesPage/ArticlesPage.tsx
+++ src/components/pages/ArticlesPage/ArticlesPage.tsx
@@ -1,31 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
-import ArticleItem from '../../block/ArticleItem';
+import ArticlesList from '../../ArticlesList';
 import Pagination from '../../block/Pagination';
+
+import { ILoadArticlesAction } from '../../../helpers/types';
+
+import { getArticles } from '../../../redux/actions';
 
 import './ArticlesPage.module.scss';
 
-const ArticlesPage: React.FC = () => (
-  <>
-    <ul>
-      <li>
-        <ArticleItem />
-      </li>
-      <li>
-        <ArticleItem />
-      </li>
-      <li>
-        <ArticleItem />
-      </li>
-      <li>
-        <ArticleItem />
-      </li>
-      <li>
-        <ArticleItem />
-      </li>
-    </ul>
-    <Pagination postsPerPage={5} postsCount={20} />
-  </>
-);
+interface IArticleState {
+  articles: ILoadArticlesAction;
+}
+
+const ArticlesPage: React.FC = () => {
+  const { articles, articlesCount } = useSelector((state: IArticleState) => state.articles);
+  const isLoading = useSelector((state: IArticleState) => state.articles.isLoading);
+  const dispatch = useDispatch();
+
+  const { page }: any = useParams();
+
+  useEffect(() => {
+    dispatch(getArticles(page ? page * 10 : 0));
+  }, [page, dispatch]);
+
+  return (
+    !isLoading && (
+      <>
+        <ArticlesList articles={articles || []} />
+        <Pagination postsPerPage={articles.length} postsCount={articlesCount} />
+      </>
+    )
+  );
+};
 
 export default ArticlesPage;
